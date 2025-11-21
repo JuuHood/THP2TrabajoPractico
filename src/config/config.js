7fb33f1765adce666dc5a1d720d4e820175fcbd8
JuuHood <juuventre@gmail.com>
2025-11-21 13:10:42 -0300

    Agregando archivos

--- src/config/config.js
+++ src/config/config.js
@@ -7,15 +7,16 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT || 3000,
   HOST: process.env.HOST || "localhost",
-  DB_PATH: process.env.DB_PATH || "./src/db/receta.db.json",
 
+  // 🔹 Supabase
   SUPABASE_URL: process.env.SUPABASE_URL,
-  SUPABASE_KEY: process.env.SUPABASE_KEY,
+  SUPABASE_KEY: process.env.SUPABASE_KEY, 
 
+  // 🔹 Credenciales para Basic Auth
   BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
   BASIC_AUTH_PASS: process.env.BASIC_AUTH_PASS,
 };
 
-// Cliente de Supabase 
+// Cliente de Supabase
 export const supabase = createClient(config.SUPABASE_URL, config.SUPABASE_KEY);
 
